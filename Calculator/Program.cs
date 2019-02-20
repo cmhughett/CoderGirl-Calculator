0093f3b3c00ef56a63935078e744359c5f3a6e91
cmhug <cmhug@LAPTOP-AR07F35P>
2019-02-19 22:56:08 -0600

    Homework

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,12 +6,56 @@ namespace Calculator
     {
         public static void Main()
         {
-            int result = 0;
+           
 
             // Create a simple calculator that asks the user to input two different numbers 
+            Console.WriteLine("Give me the first number: ");
+            string numberOneInput = Console.ReadLine();
+            int numberOne = int.Parse(numberOneInput);
+
+            Console.WriteLine("Give me the second number: ");
+            string numberTwoInput = Console.ReadLine();
+            int numberTwo = int.Parse(numberTwoInput);
+
+            Console.WriteLine("Would you like to add, subtract, multiply, or divide them?");
+            string math = Console.ReadLine();
+
+            if (math == "add")
+            {
+                int result = numberOne + numberTwo;
+                Console.WriteLine($"Total = {result}");
+                Console.ReadLine();
+                return;
+               
+            }
+
+            if (math == "subtract")
+            {
+                int result = numberOne - numberTwo;
+                Console.WriteLine($"Total = {result}");
+                Console.ReadLine();
+                return;
+            }
+
+            if (math == "multiply")
+            {
+                int result = numberOne * numberTwo;
+                Console.WriteLine($"Total = {result}");
+                Console.ReadLine();
+                return;
+            }
+
+            if (math == "divide")
+            {
+                int result = numberOne / numberTwo;
+                Console.WriteLine($"Total = {result}");
+                Console.ReadLine();
+                return;
+            }
+
             // and whether they want to add, subtract, multiply or divide them. 
 
-            Console.WriteLine($"Total = {result}");
+            
 
             Console.ReadLine();
         }
